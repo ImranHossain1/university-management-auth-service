7cc9e42f04e421e0757ae302539bd1d0ec7d1389
ImranHossain1 <imranhossain1402@gmail.com>
2023-06-17 13:27:30 +0200

    handle errors and implement zodError

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import usersRouter from '../src/app/modules/users/users.route'
-const app: Application = express()
+import express, { Application } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
+// import ApiError from './errors/ApiError'
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Application route
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', UserRoutes);
 
-//testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully!')
-})
+// //testing
+app.get('/', async () => {
+  // Promise.reject(new Error('Unhandled Promise Rejection'))
+  throw new Error('Testing Error Logger');
+});
 
-export default app
+//global error handling
+app.use(globalErrorHandler);
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,41 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorLogger, logger } from './shared/logger';
+import { Server } from 'http';
+
+// eslint-disable-next-line no-unused-vars
+process.on('uncaughtException', error => {
+  errorLogger.error(error);
+  process.exit(1);
+});
+let server: Server;
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    console.log(`Database is connected successfully`)
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is connected successfully`);
 
-    app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
-    })
+    server = app.listen(config.port, () => {
+      logger.info(`Application listening on port ${config.port}`);
+    });
   } catch (err) {
-    console.log('Failed to connect database', err)
+    errorLogger.error('Failed to connect database', err);
   }
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
-bootstrap()
+bootstrap();
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
